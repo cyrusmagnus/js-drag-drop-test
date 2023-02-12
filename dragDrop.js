0305a6d538fe01cdda6c96c113fe09a266bf2b5f
cyrusmagna <kiryasovnikita0@gmail.com>
2023-02-12 21:31:40 +0300

    start-version

---
+++ dragDrop.js
@@ -0,0 +1,41 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+for (const placeholder of placeholders) {
+  placeholder.addEventListener('dragover', dragOver);
+  placeholder.addEventListener('dragenter', dragEnter);
+  placeholder.addEventListener('dragleave', dragLeave);
+  placeholder.addEventListener('drop', dragDrop);
+}
+
+item.setAttribute('draggable', 'true');
+
+item.addEventListener('dragstart', dragStart);
+item.addEventListener('dragend', dragEnd);
+
+function dragStart(event) {
+  setTimeout(() => {
+    event.target.classList.add('hide');
+  }, 0)
+}
+
+function dragEnd(event) {
+  event.target.classList.remove('hide');
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragEnter(event) {
+  event.target.classList.add('hovered');
+}
+
+function dragLeave(event) {
+  event.target.classList.remove('hovered');
+}
+
+function dragDrop(event) {
+  event.target.classList.remove('hovered');
+  event.target.append(item);
+}
